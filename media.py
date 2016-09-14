ee491c258d14bcb6cc7fb2b0cb66811caf820216
Mathew Cortez <mcortez121@gmail.com>
2016-09-14 15:41:20 +0100

    added comments and formatting changes

--- media.py
+++ media.py
@@ -1,11 +1,15 @@
 import webbrowser
 
 class Movie():
+    """This class holds the information to create multiple movie
+    instances of this class"""
     def __init__(self, movie_title, movie_storyline, poster_image,trailer_youtube):
+        #holds variables for each movie
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
     def show_trailer(self):
+        #enables the program to open the youtube video in a browser
         webbrowser.open(self.trailer_youtube_url)
         
